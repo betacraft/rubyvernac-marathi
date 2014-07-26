142327aab4686abae1c1a5a8ed994508e92f5255
Ratnadeep Deshmane <ratnadeepdeshmane@gmail.com>
2014-07-26 13:11:03 +0530

    sample english code added in comments for function

--- examples/17_def.rb
+++ examples/17_def.rb
@@ -1,7 +1,17 @@
 require 'rubyvernac-marathi'
 
+# 
+# def namaskar(naav)
+#  puts naav
+# end
+#
+
 कार्य नमस्कार(नाव)
   छाप "नमस्कार #{नाव} !"
 शेवट
+
+
+# namaskar('somename')
+#
 
 नमस्कार('बंडू')
